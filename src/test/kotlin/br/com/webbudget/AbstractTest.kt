0407ee25f55f74dfa11c4806e560a7589cba37ca
Arthur Gregorio <arthurshakal@gmail.com>
2021-08-31 01:05:15 -0300

    fix #6

--- src/test/kotlin/br/com/webbudget/AbstractTest.kt
+++ src/test/kotlin/br/com/webbudget/AbstractTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation
 import org.junit.jupiter.api.TestMethodOrder
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
@@ -17,6 +18,7 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension::class)
 @TestMethodOrder(value = OrderAnnotation::class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class AbstractTest {
 
