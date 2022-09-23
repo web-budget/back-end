f329978d32963f833482946ba94ca7ba588525c5
Arthur Gregório <arthurshakal@gmail.com>
2022-09-23 14:39:02 -0300

    ref #15

--- src/test/kotlin/br/com/webbudget/services/registration/CostCenterServiceTest.kt
+++ src/test/kotlin/br/com/webbudget/services/registration/CostCenterServiceTest.kt
@@ -1,78 +1,126 @@
 package br.com.webbudget.services.registration
 
-import br.com.webbudget.BaseIntegrationTest
 import br.com.webbudget.domain.entities.registration.CostCenter
 import br.com.webbudget.domain.exceptions.DuplicatedPropertyException
 import br.com.webbudget.domain.services.registration.CostCenterService
+import br.com.webbudget.domain.validators.registration.CostCenterValidator
+import br.com.webbudget.domain.validators.registration.DuplicatedNameValidator
 import br.com.webbudget.infrastructure.repository.registration.CostCenterRepository
-import org.assertj.core.api.AssertionsForClassTypes
-import org.assertj.core.api.AssertionsForClassTypes.assertThat
-import org.junit.jupiter.api.AfterAll
+import io.mockk.called
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
+import io.mockk.verify
+import org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.UUID
 
-class CostCenterServiceTest : BaseIntegrationTest() {
+@ExtendWith(MockKExtension::class)
+class CostCenterServiceTest {
 
-    @Autowired
-    private lateinit var costCenterService: CostCenterService
-    @Autowired
+    @MockK
     private lateinit var costCenterRepository: CostCenterRepository
 
+    private lateinit var costCenterService: CostCenterService
+
     @BeforeEach
-    fun clearDatabase() {
-        costCenterRepository.deleteAll()
+    fun setup() {
+
+        val creatingValidators = listOf<CostCenterValidator>(
+            DuplicatedNameValidator(costCenterRepository)
+        )
+
+        val updatingValidators = listOf<CostCenterValidator>(
+            DuplicatedNameValidator(costCenterRepository)
+        )
+
+        this.costCenterService = CostCenterService(costCenterRepository, creatingValidators, updatingValidators)
     }
 
     @Test
-    fun `should create`() {
+    fun `should pass validation and save`() {
 
         val toCreate = CostCenter("To create", true)
 
-        val created = costCenterService.create(toCreate)
+        every { costCenterRepository.findByNameIgnoreCase("To create") } returns null
+        every { costCenterRepository.save(any()) } returns toCreate
 
-        assertThat(created).isEqualTo(toCreate)
-    }
-
-    @Test
-    fun `should fail if description is duplicated`() {
-
-        val toCreate = CostCenter("Duplicated", true)
         costCenterService.create(toCreate)
 
-        val duplicated = CostCenter("Duplicated", true)
-
-        AssertionsForClassTypes.assertThatThrownBy { costCenterService.create(duplicated) }
-            .isInstanceOf(DuplicatedPropertyException::class.java)
+        verify(exactly = 1) { costCenterRepository.findByNameIgnoreCase("To create") }
+        verify(exactly = 1) { costCenterRepository.save(toCreate) }
     }
 
     @Test
-    fun `should update`() {
+    fun `should fail validation and not save`() {
 
-        val toCreate = CostCenter("To update", true)
-        val created = costCenterService.create(toCreate)
+        val toCreate = CostCenter("To create", true)
 
-        created.active = false
+        every { costCenterRepository.findByNameIgnoreCase("To create") } returns toCreate
 
-        val updated = costCenterService.update(created)
+        assertThatThrownBy { costCenterService.create(toCreate) }
+            .isInstanceOf(DuplicatedPropertyException::class.java)
 
-        assertThat(updated)
-            .isEqualTo(created)
-            .hasFieldOrPropertyWithValue("active", false)
+        verify(exactly = 1) { costCenterRepository.findByNameIgnoreCase("To create") }
+        verify { costCenterRepository.save(toCreate) wasNot called }
+    }
+
+    @Test
+    fun `should pass validation and update`() {
+
+        val externalId = UUID.randomUUID()
+        val toUpdate = CostCenter("To update", true)
+            .apply {
+                this.id = 1L
+                this.externalId = externalId
+            }
+
+        every { costCenterRepository.findByNameIgnoreCaseAndExternalIdNot("To update", externalId) } returns null
+        every { costCenterRepository.save(any()) } returns toUpdate
+
+        costCenterService.update(toUpdate)
+
+        verify(exactly = 1) { costCenterRepository.findByNameIgnoreCaseAndExternalIdNot(any(), any()) }
+        verify(exactly = 1) { costCenterRepository.save(toUpdate) }
+    }
+
+    @Test
+    fun `should fail validation and not update`() {
+
+        val externalId = UUID.randomUUID()
+        val toUpdate = CostCenter("To update", true)
+            .apply {
+                this.id = 1L
+                this.externalId = externalId
+            }
+
+        every {
+            costCenterRepository.findByNameIgnoreCaseAndExternalIdNot("To update", externalId)
+        } returns toUpdate
+        every { costCenterRepository.save(any()) } returns toUpdate
+
+        assertThatThrownBy { costCenterService.update(toUpdate) }
+            .isInstanceOf(DuplicatedPropertyException::class.java)
+
+        verify(exactly = 1) { costCenterRepository.findByNameIgnoreCaseAndExternalIdNot(any(), any()) }
+        verify { costCenterRepository.save(toUpdate) wasNot called }
     }
 
     @Test
     fun `should delete`() {
 
-        val toCreate = CostCenter("To delete", true)
-        val created = costCenterService.create(toCreate)
+        val toDelete = CostCenter("To delete", true)
 
-        costCenterService.delete(created)
+        every { costCenterRepository.delete(any()) } just runs
 
-        val found = costCenterRepository.findByExternalId(created.externalId!!)
+        costCenterService.delete(toDelete)
 
-        assertThat(found).isNull()
+        verify(exactly = 1) { costCenterRepository.delete(toDelete) }
     }
 
     @Test
