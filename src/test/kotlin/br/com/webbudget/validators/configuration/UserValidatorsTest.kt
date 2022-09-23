beec18991b5f2038aa2bf1d5517d873ae048eba4
Arthur Gregório <arthurshakal@gmail.com>
2022-09-23 10:49:51 -0300

    ref #15

--- src/test/kotlin/br/com/webbudget/validators/configuration/UserValidatorsTest.kt
+++ src/test/kotlin/br/com/webbudget/validators/configuration/UserValidatorsTest.kt
@@ -2,37 +2,66 @@ package br.com.webbudget.validators.configuration
 
 import br.com.webbudget.BaseIntegrationTest
 import br.com.webbudget.domain.entities.configuration.User
-import br.com.webbudget.domain.exceptions.BusinessException
+import br.com.webbudget.domain.exceptions.DuplicatedPropertyException
 import br.com.webbudget.domain.validators.configuration.DuplicatedEmailValidator
 import br.com.webbudget.infrastructure.repository.configuration.UserRepository
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.assertj.core.api.Assertions.assertThatNoException
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertDoesNotThrow
-import org.junit.jupiter.api.assertThrows
-import org.springframework.beans.factory.annotation.Autowired
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.UUID
 
+@ExtendWith(MockKExtension::class)
 class UserValidatorsTest : BaseIntegrationTest() {
 
-    @Autowired
+    @MockK
     lateinit var userRepository: UserRepository
 
-    @Autowired
     lateinit var duplicatedEmailValidator: DuplicatedEmailValidator
+
+    @BeforeEach
+    fun setup() {
+        this.duplicatedEmailValidator = DuplicatedEmailValidator(userRepository)
+    }
 
     @Test
     fun `should fail when duplicated email`() {
 
-        val user = User("Checking", "checking@teste.com", "123", false, listOf())
-        userRepository.save(user)
+        val duplicated = User("Duplicated", "test@test.com", "123", false, listOf())
+            .apply {
+                this.id = 1L
+                this.externalId = UUID.randomUUID()
+            }
 
-        val other = User("Checking", "checking@teste.com", "123", false, listOf())
-        assertThrows<BusinessException>("users.error.duplicated-email") {
-            duplicatedEmailValidator.validate(other)
-        }
+        every { userRepository.findByEmail("test@test.com") } returns duplicated
+
+        val toValidate = User("Duplicated", "test@test.com", "123", false, listOf())
+
+        assertThatThrownBy { duplicatedEmailValidator.validate(toValidate) }
+            .isInstanceOf(DuplicatedPropertyException::class.java)
+            .hasMessage("user.email")
     }
 
     @Test
     fun `should pass when same entity is checked for duplicated email`() {
-        val saved = userRepository.save(User("Duplicated", "duplicated@teste.com", "123", false, listOf()))
-        assertDoesNotThrow { duplicatedEmailValidator.validate(saved) }
+
+        val externalId = UUID.randomUUID()
+
+        val notDuplicated = User("Not Duplicated", "test@test.com", "123", false, listOf())
+            .apply {
+                this.id = 1L
+                this.externalId = externalId
+            }
+
+        every {
+            userRepository.findByEmailAndExternalIdNot("test@test.com", externalId)
+        } returns null
+
+        assertThatNoException()
+            .isThrownBy { duplicatedEmailValidator.validate(notDuplicated) }
     }
 }
