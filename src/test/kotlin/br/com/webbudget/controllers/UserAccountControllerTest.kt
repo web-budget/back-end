5a2f80429de2392b3e9d02b55deacf72de7f4963
Arthur Gregorio <arthurshakal@gmail.com>
2023-05-17 18:35:30 -0300

    ref #28

--- src/test/kotlin/br/com/webbudget/controllers/UserAccountControllerTest.kt
+++ src/test/kotlin/br/com/webbudget/controllers/UserAccountControllerTest.kt
@@ -2,10 +2,15 @@ package br.com.webbudget.controllers
 
 import br.com.webbudget.BaseControllerIntegrationTest
 import br.com.webbudget.application.controllers.UserAccountController
+import br.com.webbudget.domain.services.administration.UserAccountService
+import com.ninjasquad.springmockk.MockkBean
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 
 @WebMvcTest(UserAccountController::class)
 class UserAccountControllerTest : BaseControllerIntegrationTest() {
+
+    @MockkBean
+    private lateinit var userAccountService: UserAccountService
 
     // TODO create all controller testes here!
 
