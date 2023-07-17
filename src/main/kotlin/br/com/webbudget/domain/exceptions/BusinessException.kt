458cfcd2ce8bbfceefa06a1ffba6e44fb45f5a99
Arthur Gregorio <arthurshakal@gmail.com>
2023-07-17 00:07:20 -0300

    fix #54

--- src/main/kotlin/br/com/webbudget/domain/exceptions/BusinessException.kt
+++ src/main/kotlin/br/com/webbudget/domain/exceptions/BusinessException.kt
@@ -3,4 +3,5 @@ package br.com.webbudget.domain.exceptions
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.BAD_REQUEST
 
-open class BusinessException(message: String, val detail: String, val httpStatus: HttpStatus = BAD_REQUEST) : RuntimeException(message)
+open class BusinessException(message: String, val detail: String, val httpStatus: HttpStatus = BAD_REQUEST) :
+    RuntimeException(message)
