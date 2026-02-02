af7abcf421dfdf22ad0989b90b219e2d54bb5207
Arthur Gregorio <arthurshakal@gmail.com>
2026-02-01 23:59:10 -0300

    fix #93: removed mapstruct

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,16 +5,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // spring
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
 
     // detekt
-    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("dev.detekt") version "2.0.0-alpha.1"
 
     // kotlin things
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.spring") version "2.0.21"
-    kotlin("plugin.jpa") version "2.0.21"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.spring") version "2.3.0"
+    kotlin("plugin.jpa") version "2.3.0"
 }
 
 group = "br.com.webbudget"
