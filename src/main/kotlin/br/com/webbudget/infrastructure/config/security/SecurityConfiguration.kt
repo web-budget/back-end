6d8d643900a31b9d8c585eec12782cd8352a9ae6
Arthur Gregorio <arthurshakal@gmail.com>
2026-03-29 22:07:23 -0300

    ref #108: remove import

--- src/main/kotlin/br/com/webbudget/infrastructure/config/security/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/webbudget/infrastructure/config/security/SecurityConfiguration.kt
@@ -19,7 +19,6 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm
 import org.springframework.security.oauth2.jwt.JwtDecoder
 import org.springframework.security.oauth2.jwt.JwtEncoder
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
